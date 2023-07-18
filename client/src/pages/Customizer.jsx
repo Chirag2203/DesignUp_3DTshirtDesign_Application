a03e288c49f6ee263dc18dd4f88f33fc16b5dd29
Chirag Rajput <chirag.3r@gmail.com>
2023-07-18 10:28:49 +0530

    stable 1.2

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -183,17 +183,19 @@ useEffect(() => {
             {/* add the reset function here */}
             
             <ProfileButton/>
-            <button   className='gb-btn w-9  rounded-3xl'>
-            <svg viewBox="0 0 16 16" class="bi bi-arrow-repeat" fill="currentColor"  xmlns="http://www.w3.org/2000/svg" className='w-7 h-7 ml-1 refresh-btn'>
-              <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"></path>
-              <path d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z" fill-rule="evenodd"></path>
-            </svg>  </button>
+            {/* BOOKMARK */}
             <button class="bookmarkBtn">
               <span class="IconContainer"> 
                 <svg viewBox="0 0 384 512" height="0.9em" class="icon"><path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"></path></svg>
               </span>
               <p class="text">Save</p>
             </button>
+            {/* REFRESH */}
+            <button   className='gb-btn w-9  rounded-3xl'>
+            <svg viewBox="0 0 16 16" class="bi bi-arrow-repeat" fill="currentColor"  xmlns="http://www.w3.org/2000/svg" className='w-7 h-7 ml-1 refresh-btn'>
+              <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"></path>
+              <path d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z" fill-rule="evenodd"></path>
+            </svg>  </button>
             {/* <button  onClick={() => state.intro = true}  className='gb-btn'> Sa </button> */}
             <DownloadButton handleClick={captureCanvasAndDownload} /> {/* Use the custom DownloadButton */}
             
